31edc1c41be169439e1c4f008faae82621f0c16b
Enal Jo <enalkristo@gmail.com>
2021-11-19 16:33:50 +0800

    Coba Upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/logo/logo.png">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Landing ccODSK</title>
+</head>
+<body>
+    
+    <div class="belakang">
+
+        <video autoplay loop muted id="backVideo">
+            <source src="assets/video/video1.mp4" type="video/mp4">
+        </video>
+        <div class="row">
+            <div class="col">
+                <div class="logopemprov">
+                    <img src="assets/logo/logo-pemprov1.png" alt="" srcset="">
+                </div>
+            </div>
+
+            <div class="col">
+                <form class="box" action="#">
+                    <img class="logo1" src="assets/logo/logo2.png" alt="" srcset="">
+                    <img class="lintasarta" src="assets/logo/lintasarta1.png" alt="" srcset="">
+                    <h1>SELAMAT DATANG</h1>
+                    <input type="text" name="" id="" placeholder="USERNAME">
+                    <br>
+                    <input type="password" name="" id="" placeholder="PASSWORD"> 
+                    <br>
+                    <input type="submit" value="LOGIN">
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
